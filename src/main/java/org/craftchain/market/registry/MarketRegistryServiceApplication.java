246063bce251c89e9d36e075ce45f7616ce9ed1d
Anicet Togue <atogue@gmail.com>
2021-01-14 12:34:13 +0100

    init project

---
+++ src/main/java/org/craftchain/market/registry/MarketRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketRegistryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketRegistryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/craftchain/market/registry/MarketRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.craftchain.market.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketRegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
