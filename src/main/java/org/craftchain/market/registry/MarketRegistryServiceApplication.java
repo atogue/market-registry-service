b38f1e12c2c31b6b755c4edf527aca0db6307cd9
Anicet Togue <atogue@gmail.com>
2021-01-14 14:22:53 +0100

    enable eureka server

--- src/main/java/org/craftchain/market/registry/MarketRegistryServiceApplication.java
+++ src/main/java/org/craftchain/market/registry/MarketRegistryServiceApplication.java
@@ -2,8 +2,10 @@ package org.craftchain.market.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MarketRegistryServiceApplication {
 
     public static void main(String[] args) {
